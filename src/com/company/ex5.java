4caf99ccac2118398e81e2421ec22642dd01ee33
Daniel <daniel.barczynski@onet.pl>
2022-03-25 11:10:53 +0100

    ex5 done

---
+++ src/com/company/ex5.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+
+public class ex5 {
+    public static void main(String[] args) {
+
+        System.out.println("Krakow time:");
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm:ss");
+        LocalTime localTime = LocalTime.now();
+        System.out.println(dtf.format(localTime));
+
+        System.out.println("Sydney time:");
+        ZonedDateTime now = ZonedDateTime.now();
+        ZonedDateTime sydney = now.withZoneSameInstant(ZoneId.of("Australia/Sydney"));
+        System.out.println(dtf.format(sydney));
+    }
+}
