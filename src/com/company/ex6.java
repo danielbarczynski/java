5493309a14adabca465b9da55770bbdc6b44426c
Daniel <daniel.barczynski@onet.pl>
2022-03-25 11:45:12 +0100

    ex6 done

---
+++ src/com/company/ex6.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class ex6 {
+    public static void main(String[] args) {
+        String[] lines = splitLines("line 1\nline 2\nline 3");
+        for (String line : lines) {
+            System.out.println(line);
+        }
+    }
+
+    static String[] splitLines(String text) {
+        return text.split("\r\n|\n\r|\n|\r");
+    }
+}
