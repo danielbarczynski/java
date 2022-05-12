7bd1f5f6b401f9063ddb19ffcf9d6a460bf1f2af
Daniel PC <daniel.barczynski@onet.pl>
2022-05-12 20:34:57 +0200

    ex6 done

--- lab2/src/main/java/com/example/lab2/controllers/UserController.java
+++ lab2/src/main/java/com/example/lab2/controllers/UserController.java
@@ -2,10 +2,9 @@ package com.example.lab2.controllers;
 import com.example.lab2.models.UserEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.HashMap;
 import java.util.Map;
 
 @Controller
@@ -22,7 +21,7 @@ public class UserController {
     @ResponseBody
     public Object apiUsers(@RequestParam(value = "page-number", defaultValue = "1") Integer pageNumber,
                            @RequestParam(value = "page-size", defaultValue = "20") Integer pageSize) {
-        return this.userService.getUserPage(pageNumber, pageSize);
+        return userService.getUserPage(pageNumber, pageSize);
     }
 
     @RequestMapping(
@@ -33,7 +32,7 @@ public class UserController {
     )
     @ResponseBody
     public UserEntity createUser(@RequestBody UserEntity user) {
-        return this.userService.createUser(user);
+        return userService.createUser(user);
     }
 
     @RequestMapping(
@@ -44,7 +43,7 @@ public class UserController {
 
     @ResponseBody
     public UserEntity getUser(@PathVariable("id") int id) {
-        return this.userService.getUser(id);
+        return userService.getUser(id);
     }
 
     @RequestMapping(
@@ -54,8 +53,11 @@ public class UserController {
     )
 
     @ResponseBody
-    public UserEntity updateUser(@PathVariable("id") int id, @RequestBody UserEntity user) {
-        return this.userService.updateUser(id, user);
+    public ResponseEntity<UserEntity> updateUser(@PathVariable("id") int id, @RequestBody UserEntity user) {
+        if(userService.getUser(id) == null)
+            return ResponseEntity.notFound().build();
+        else
+            return ResponseEntity.ok(userService.updateUser(id, user));
     }
 
     @RequestMapping(
@@ -65,8 +67,11 @@ public class UserController {
     )
 
     @ResponseBody
-    public Map<String, Boolean> removeUser(@PathVariable("id") int id) {
-        return this.userService.removeUser(id);
+    public ResponseEntity<Map<String, Boolean>> removeUser(@PathVariable("id") int id) {
+        if(userService.getUser(id) == null)
+            return ResponseEntity.notFound().build();
+        else
+            return ResponseEntity.ok(userService.removeUser(id));
     }
 }
 
